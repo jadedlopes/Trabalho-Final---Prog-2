4f5028b47116dfddbd8ff32e737ab4990a5b1144
Winggel <hazaelsb97@gmail.com>
2017-11-29 15:47:47 -0200

    soma adicionado

--- Final_Prog2/main.c
+++ Final_Prog2/main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int soma(int a, int b)
+{
+    return a + b;
+}
+
 int main()
 {
     int i;
+    int u = 0;
 
-    for(i = 0; i < 33; i++) {
-        printf("%d\n", i);
+    for(i = 0; i < 17; i++) {
+        u = soma(u, i);
+        printf("%d\n", u);
     }
 
     return 0;
